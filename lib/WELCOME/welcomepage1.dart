a0151818c5320e831821a32c15967ec5c7aba5b4
Prionty Saha <priontysaha003@gmail.com>
2024-05-27 20:36:04 +0600

    splashscreen

--- lib/WELCOME/welcomepage1.dart
+++ lib/WELCOME/welcomepage1.dart
@@ -3,5 +3,10 @@
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 
 }
