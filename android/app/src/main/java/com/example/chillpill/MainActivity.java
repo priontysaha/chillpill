530a3e9a83edb0d7bbbb3d220cd56fbbf114cfd3
Prionty Saha <priontysaha003@gmail.com>
2024-05-27 20:09:03 +0600

    1st work

---
+++ android/app/src/main/java/com/example/chillpill/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.chillpill;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/WELCOME/welcomepage1.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget{
+
+}
