f25aca44b34d9f15d537962525026acb8f2684aa
Prionty Saha <priontysaha003@gmail.com>
2024-09-14 21:52:07 +0600

    spaceremoved

--- lib/PAGES/medicine_type_page.dart
+++ lib/PAGES/medicine_type_page.dart
@@ -17,7 +17,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
-
 import '../models/medicine_type.dart';
 
 class medicine_type extends StatefulWidget {
